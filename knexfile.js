9d450bc2e2d507e496d54d555257aaacd0fb29c7
Ankitagole <ankita17@navgurukul.org>
2019-02-17 11:01:29 +0530

    intial commit

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+
+    migrations: { tableName: 'knex_migrations' },
+    seeds: { tableName: './seeds' },
+
+    client: 'mysql',
+    connection: {
+
+        host: 'localhost',
+
+        user: 'root',
+        password: 'navgurukul',
+
+        database: 'user',
+        charset: 'utf8',
+
+    }
+
+}
+};
